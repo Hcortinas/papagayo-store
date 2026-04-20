86095e746dcbaf3278d068aaa4b3da11d08e1943
Hcortinas <hugocortinasgonzalez@outlook.com>
2026-04-20 15:27:34 -0600

    Add full PHP/MySQL sales system - initial commit

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,13 +1,11 @@
 <?php
-$host = "localhost";
-$usuario = "museopap_usertienda";
-$contrasena = "Papagayo2024!";
-$bd = "museopap_tienda_papagayo";
+$host     = getenv('DB_HOST')     ?: 'localhost';
+$usuario  = getenv('DB_USER')     ?: '';
+$contrasena = getenv('DB_PASS')   ?: '';
+$bd       = getenv('DB_NAME')     ?: '';
 
-// Crear conexión
 $conn = new mysqli($host, $usuario, $contrasena, $bd);
 
-// Verificar conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
